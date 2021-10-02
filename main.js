407bc717bb65c7320647fc26636b868d6bf37f47
hunseok-Y <gnstjr5911@gmail.com>
2021-10-03 01:22:11 +0900

    html 수정 팔로잉 생성
    팔로잉 생성 및 js 수정

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 "use strict";
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 
-const swiper = new Swiper(".mySwiper", {
-  spaceBetween: 2,
+const swiper = new Swiper(".visular__inner", {
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+const button = document.querySelector(".info-menu");
+
+button.addEventListener("click", () => {
+  const dropdown = document.querySelector(".info__discription");
+  dropdown.style.display = "block";
+});
+
+button.addEventListener("blur", () => {
+  const dropdown = document.querySelector(".info__discription");
+  dropdown.style.display = "";
 });
