961ffd82c1f990d9bfbe1372a9892c45a15511ee
hunseok-Y <gnstjr5911@gmail.com>
2021-10-08 17:56:34 +0900

    swiper.js 수정 및 class 수정
    swiper 동적으로 구현 및 class 수정

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 "use strict";
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 
+const bullet = ["지식공간", "팔로잉"];
+const swiper = new Swiper(".main-view__swiper", {
+  spaceBetween: 20,
+  speed: 1500,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<div class="' +
+        className +
+        '"><span>' +
+        bullet[index] +
+        "</span></div>"
+      );
+    },
+  },
+  navigation: {
+    prevEl: ".news-btn",
+    nextEl: ".following-btn",
+  },
+});
+
 const button = document.querySelector(".content__info-icon");
 
 button.addEventListener("click", () => {
@@ -11,22 +35,4 @@ button.addEventListener("click", () => {
 button.addEventListener("blur", () => {
   const dropdown = document.querySelector(".content__discription");
   dropdown.style.display = "";
-});
-
-const bullet = ["지식공간", "팔로잉"];
-const swiper = new Swiper(".main-view__swiper", {
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (text, className) {
-      return (
-        '<div class="' + className + '"><span>' + bullet[text] + "</span></div>"
-      );
-    },
-  },
-  navigation: {
-    prevEl: ".news-btn",
-    nextEl: ".following-btn",
-  },
 });
