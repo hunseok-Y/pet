4d3d3db1a396040642380918b48ee4a525ad2f35
hunseok-Y <gnstjr5911@gmail.com>
2021-10-08 16:06:24 +0900

    css 수정 및 js 수정
    swiper 수정 및 css 수정

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 "use strict";
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 
-const swiper = new Swiper(".mySwiper", {
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
 const button = document.querySelector(".content__info-icon");
 
 button.addEventListener("click", () => {
@@ -19,4 +11,22 @@ button.addEventListener("click", () => {
 button.addEventListener("blur", () => {
   const dropdown = document.querySelector(".content__discription");
   dropdown.style.display = "";
+});
+
+const bullet = ["지식공간", "팔로잉"];
+const swiper = new Swiper(".main-view__swiper", {
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (text, className) {
+      return (
+        '<div class="' + className + '"><span>' + bullet[text] + "</span></div>"
+      );
+    },
+  },
+  navigation: {
+    prevEl: ".news-btn",
+    nextEl: ".following-btn",
+  },
 });
