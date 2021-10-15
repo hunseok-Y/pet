c1b2dd07fb6577bce2f509fb5ddeeee196fafc8c
hunseok-Y <gnstjr5911@gmail.com>
2021-10-15 13:26:22 +0900

    records 생성 수정 및 이미지 추가
    img 추가 및 records  생성

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const swiper = new Swiper(".main-view__swiper", {
   spaceBetween: 20,
   speed: 1500,
   loop: true,
+  // autoHeight은 slide height를 자동으로 맞춰줌
+  autoHeight: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -25,16 +27,33 @@ const swiper = new Swiper(".main-view__swiper", {
   },
 });
 
-const button = document.querySelector(".content__info-icon");
+// const button = document.querySelector(".content__info-icon");
 
-button.addEventListener("click", () => {
-  const dropdown = document.querySelector(".content__discription");
-  dropdown.style.display = "block";
-});
+// button.addEventListener("click", () => {
+//   const dropdown = document.querySelector(".content__discription");
+//   dropdown.style.display = "block";
+// });
 
-button.addEventListener("blur", () => {
-  const dropdown = document.querySelector(".content__discription");
-  dropdown.style.display = "";
-});
+// button.addEventListener("blur", () => {
+//   const dropdown = document.querySelector(".content__discription");
+//   dropdown.style.display = "";
+// });
+const buttons = document.querySelectorAll(".content__info-icon");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (event) => {
+    const { target } = event;
+    const parent = target.closest(".content__info");
+    const dropdown = parent.querySelector(".content__discription");
+    dropdown.classList.add("is-active");
+  });
+
+  buttons[i].addEventListener("blur", () => {
+    const { target } = event;
+    const parent = target.closest(".content__info");
+    const dropdown = parent.querySelector(".content__discription");
+    dropdown.classList.remove("is-active");
+  });
+}
 
 AOS.init();
---
+++ records/main.js
@@ -0,0 +1,13 @@
+new Chartist.Pie(
+  ".ct-chart",
+  {
+    series: [1000, 600],
+  },
+  {
+    donut: true,
+    donutWidth: 10,
+    donutSolid: true,
+    startAngle: 270,
+    showLabel: true,
+  }
+);
